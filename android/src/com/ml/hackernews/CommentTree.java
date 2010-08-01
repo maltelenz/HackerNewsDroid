306d62a7457537a054cec5ff1c80fd6898194050
Malte Lenz <tyskis@gmail.com>
2010-08-01 17:50:34 +0200

    enable showing comments on 'ask HN' type of posts. XML cleanup

---
+++ android/src/com/ml/hackernews/CommentTree.java
@@ -0,0 +1,166 @@
+package com.ml.hackernews;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+/**
+ * Data structure representing a tree of comments for one post.
+ * @author Malte Lenz
+ *
+ */
+public class CommentTree {
+
+	private static final String TAG = "HN->CommentTree";
+
+	private static final String KEY_PARENT = "parent";
+	private static final String KEY_ID = "id";
+	private static final String KEY_TEXT = "text";
+	private static final String KEY_POINTS = "points";
+	private static final String KEY_POSTER = "poster";
+
+	private ArrayList<Comment> comments;
+
+	/**
+	 * Add a comment to the tree.
+	 * @param json structure of the comment
+	 */
+	public final void addComment(final JSONObject json) {
+		String poster;
+		String points;
+		String text;
+		String id;
+		String parent;
+		try {
+			poster = json.getString(KEY_POSTER);
+			points = json.getString(KEY_POINTS);
+			text = json.getString(KEY_TEXT);
+			id = json.getString(KEY_ID);
+			parent = json.getString(KEY_PARENT);
+		} catch (JSONException e) {
+			Log.e(TAG, "Could not decode comment: " + e.toString());
+			return;
+		}
+
+		Integer parentComment;
+		if (parent.equals("0")) {
+			parentComment = -1;
+		} else {
+			parentComment = getComment(parent);
+		}
+		Log.d(TAG, "parentComment: " + parentComment);
+		comments.add(new Comment(id, text, points, poster, parentComment));
+	}
+
+
+	/**
+	 * Fetch a comment position by id from the tree.
+	 * @param id of the parent
+	 * @return the comment position
+	 */
+	private Integer getComment(final String id) {
+		Iterator<Comment> commentitr = comments.iterator();
+		int i = 0;
+		while (commentitr.hasNext()) {
+			Comment c = commentitr.next();
+			if (c.id.equals(id)) {
+				return i;
+			}
+			i++;
+		}
+		return null;
+	}
+
+	/**
+	 * Fetch a comment on position in list.
+	 * @param position in the list
+	 * @return the comment
+	 */
+	public final Comment getCommentInPosition(final int position) {
+		return comments.get(position);
+	}
+
+	/**
+	 * Represents one comment and its location in the tree.
+	 * @author Malte Lenz
+	 *
+	 */
+	final class Comment {
+
+		private String poster;
+		private String points;
+		private String text;
+		private String id;
+		private int parent;
+
+		/**
+		 * Create a new comment.
+		 * @param newId given id
+		 * @param newText text of the comment
+		 * @param newPoints nr of points
+		 * @param newPoster poster of the comment
+		 * @param newParent position in the list of parent of the comment
+		 */
+		private Comment(final String newId, final String newText, final String newPoints, final String newPoster, final int newParent) {
+			id = newId;
+			text = newText;
+			points = newPoints;
+			poster = newPoster;
+			parent = newParent;
+		}
+
+		/**
+		 * Fetch text of the comment.
+		 * @return text
+		 */
+		public String getText() {
+			return text;
+		}
+
+		/**
+		 * Fetch points of the comment.
+		 * @return points
+		 */
+		public String getPoints() {
+			return points;
+		}
+
+		/**
+		 * Fetch author of the comment.
+		 * @return author
+		 */
+		public String getAuthor() {
+			return poster;
+		}
+
+		/**
+		 * Returns the number of levels down this comment is in the tree.
+		 * @return number of levels
+		 */
+		public Integer getIndent() {
+			if (parent == -1) {
+				return 0;
+			}
+			return getCommentInPosition(parent).getIndent() + 1;
+		}
+	}
+
+	/**
+	 * Empty default constructor.
+	 */
+	public CommentTree() {
+		 comments = new ArrayList<Comment>();
+	}
+
+	/**
+	 * Return a copy of our array.
+	 * @return our comment array
+	 */
+	public final ArrayList<Comment> getArray() {
+		return comments;
+	}
+}
---
+++ android/src/com/ml/hackernews/Comments.java
@@ -0,0 +1,228 @@
+package com.ml.hackernews;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Show a post and its comments.
+ * @author Malte Lenz
+ *
+ */
+public class Comments extends Activity {
+	/** Tag for logging. */
+	private static final String TAG = "HN->Comments";
+
+	/**
+	 * Name of the id in the intent extras.
+	 */
+	public static final String KEY_INTENT_ID = "id";
+
+	private static final String KEY_POSTCOMMENTS = "postcomments";
+
+	private static final String KEY_POSTNAME = "postname";
+
+	private static final String KEY_POSTTEXT = "posttext";
+
+	private static final String KEY_POSTPOINTS = "postpoints";
+	private ProgressDialog busy;
+
+	private Object nrComments;
+
+	private Object name;
+
+	private Object text;
+
+	private Object points;
+
+	private ListView commentView;
+
+	/**
+	 * Called on creation.
+	 * @param savedInstanceState from last time
+	 */
+    @Override
+	public final void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.comments);
+
+        Integer id = getIntent().getExtras().getInt(KEY_INTENT_ID);
+		//prepare a progress dialog
+		busy = new ProgressDialog(this);
+		busy.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		busy.setMessage("Fetching comments...");
+		busy.setCancelable(false);
+
+		commentView = (ListView) findViewById(R.id.comments_list);
+		Log.d(TAG, "commentView: " + commentView);
+		//Fetch comments from server
+		new GetComments().execute(id);
+		busy.show();
+    }
+
+	/**
+	 * Fetches the given page from hackernews via the server.
+	 * @author malte
+	 *
+	 */
+	private class GetComments extends AsyncTask<Integer, Object, String> {
+
+		@Override
+		protected String doInBackground(final Integer... itemid) {
+			//prepare request
+
+			String url = Config.baseUrl + "item/" + itemid[0];
+			final ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			final HttpClient client = new DefaultHttpClient();
+			final HttpGet get = new HttpGet(url);
+			Log.d(TAG, "Fetching url: " + get.getURI().toString());
+			String commentResults = null;
+			//call server
+			try {
+				commentResults = client.execute(get, responseHandler);
+			} catch (ClientProtocolException e) {
+				Log.e(TAG, "ClientProtocolException while searching: " + e.toString());
+			} catch (IOException e) {
+				Log.e(TAG, "IOException while searching: " + e.toString());
+			} catch (java.lang.IllegalArgumentException e) {
+				Log.e(TAG, "IllegalArgumentException while searching: " + e.toString());
+			}
+			return commentResults;
+		}
+
+		@Override
+		protected void onPostExecute(final String results) {
+			Log.d(TAG, "Got result: " + results);
+			busy.hide();
+			if (results != null) {
+				showResults(results);
+			} else {
+				Log.w(TAG, "Something went wrong in server contact.");
+				//show message to the user about this
+				Toast.makeText(getBaseContext(), "Could not contact server", Toast.LENGTH_LONG).show();
+			}
+			super.onPostExecute(results);
+		}
+	}
+
+
+	/**
+	 * Updates the listView of results the user sees.
+	 * @param searchResults	a string in json format with search results
+	 */
+	public final void showResults(final String searchResults) {
+		CommentTree resultList = new CommentTree();
+		try {
+			final JSONArray jsonResults = new JSONArray(searchResults);
+
+			Log.d(TAG, "Number of items found: " + jsonResults.length());
+			//check if we have any hits
+			if (jsonResults.length() == 0) {
+				Toast.makeText(getBaseContext(), "No items found", Toast.LENGTH_LONG).show();
+			}
+
+			for (int i = 0; i < jsonResults.length(); i = i + 1) {
+				JSONObject json = jsonResults.getJSONObject(i);
+				try {
+					//check if this is info about the post
+					text = json.getString(KEY_POSTTEXT);
+					nrComments = json.getString(KEY_POSTCOMMENTS);
+					name = json.getString(KEY_POSTNAME);
+					points = json.getString(KEY_POSTPOINTS);
+				} catch (JSONException e) {
+					//this seems to be a comment
+					resultList.addComment(json);
+				}
+			}
+		} catch (JSONException e) {
+			Log.e(TAG, "Could not decode results: " + e.toString());
+		}
+
+		final CommentAdapter resultAdapter = new CommentAdapter(this, R.layout.post_item, resultList.getArray(), resultList);
+		Log.d(TAG, "view: " + commentView + " adapter: " + resultAdapter);
+		commentView.setAdapter(resultAdapter);
+	}
+
+	/**
+	 * Adapter used for showing the list comments.
+	 * @author Malte Lenz
+	 *
+	 */
+	private class CommentAdapter extends ArrayAdapter<CommentTree.Comment> {
+
+		private static final int LEVEL_INDENT = 10;
+		private CommentTree commentTree;
+
+		/**
+		 * Constructor which saves the list of beers to a local field.
+		 * @param context calling context
+		 * @param textViewResourceId what text resource (xml file) to use for display
+		 * @param arrayList list of comments
+		 * @param tree a tree copy of list of comments
+		 */
+		public CommentAdapter(final Context context, final int textViewResourceId, final ArrayList<CommentTree.Comment> arrayList, final CommentTree tree) {
+			super(context, textViewResourceId, arrayList);
+			commentTree = tree;
+		}
+
+		@Override
+		public View getView(final int position, final View convertView, final ViewGroup parent) {
+			//Log.d(TAG, "Showing comment in position: " + position);
+			View v = convertView;
+			if (v == null) {
+				final LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				v = vi.inflate(R.layout.comment_item, null);
+			}
+			final CommentTree.Comment c = commentTree.getCommentInPosition(position);
+			Log.d(TAG, "Comment fetched: " + c.toString());
+			if (c != null) {
+				final TextView ct = (TextView) v.findViewById(R.id.comment_text);
+				final TextView cp = (TextView) v.findViewById(R.id.comment_points);
+				final TextView ca = (TextView) v.findViewById(R.id.comment_author);
+				if (ct != null) {
+					ct.setText(c.getText());
+				}
+				if (cp != null) {
+					cp.setText(c.getPoints());
+				}
+				if (ca != null) {
+					ca.setText(c.getAuthor());
+				}
+			}
+			v.setPadding(c.getIndent() * LEVEL_INDENT, 0, 0, 0);
+			return v;
+		}
+
+	}
+
+	@Override
+	protected final void onDestroy() {
+		//dismiss dialog
+		busy.dismiss();
+		super.onDestroy();
+	}
+
+}
